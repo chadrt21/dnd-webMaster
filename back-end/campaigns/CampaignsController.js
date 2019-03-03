ff8aec27a9bef51c41ad179dec8f181894b84edf
Joseph Stewart <joseph.s@nationalschoolproject.com>
2019-03-03 13:08:32 -0800

    Adds route to check if a campaign exists

--- back-end/campaigns/CampaignsController.js
+++ back-end/campaigns/CampaignsController.js
@@ -59,3 +59,32 @@ export const createNewCampaign = async (path, query, user, connection, body) => 
 		campaignID: campaignResults.insertId,
 	};
 };
+
+/**
+ * @description Finds if a specific campaign exists in the database returns
+ * the result as a boolean in the return JSON by the property "exists"
+ */
+export const checkIfCampaignExists = async (path, query, user, connection) => {
+	const { campaignID } = path;
+	const { id } = user;
+
+	const result = await promiseQuery(
+		connection,
+		`
+		SELECT 
+			COUNT(campaign.campaignID) as result
+		FROM
+			campaignlist
+				JOIN
+			campaign ON campaignlist.campaignID = campaign.campaignID
+		WHERE
+			campaignlist.dmID = :id
+			AND campaign.campaignID = :campaignID
+		`,
+		{ campaignID, id }
+	);
+
+	return {
+		exists: result[0].result > 0,
+	};
+};
--- back-end/campaigns/index.js
+++ back-end/campaigns/index.js
@@ -5,6 +5,9 @@ import {
 import * as campaignRoutes from './CampaignsController';
 
 export default app => {
+	app.route('/api/campaigns/:campaignID')
+		.get(asRouteFunction(campaignRoutes.checkIfCampaignExists, true));
+
 	app.route('/api/campaigns')
 		.get(asRouteFunction(campaignRoutes.getAllCampaigns, true));
 
