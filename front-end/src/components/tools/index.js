465d6dbeaebf459ac9f915400646d8f687a917a6
achiaseed <ashley.m.cheah@biola.edu>
2019-03-07 18:18:02 -0800

    - added more to the diceroller program. It works but it isn't styled.

--- front-end/src/components/tools/index.js
+++ front-end/src/components/tools/index.js
@@ -3,6 +3,7 @@ import ExampleTool from './example';
 import Character from './character';
 import SearchTool from './search';
 import Notes from './notes';
+import diceRoller from './dice-roller';
 
 /**
  * Add an object to the array below in the following format:
@@ -44,5 +45,11 @@ export default [
 		displayName: 'Notes Tool',
 		defaultLabel: 'Notes',
 		component: Notes,
+	},
+	{
+		name: 'diceRoller',
+		displayName: 'Dice Roll',
+		defaultLabel: 'Dice Roller',
+		component: diceRoller,
 	},
 ];
