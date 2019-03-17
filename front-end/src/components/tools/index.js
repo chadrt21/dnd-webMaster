c6cf46e2fb6a247913636535e25891321c98b30c
achiaseed <ashley.m.cheah@biola.edu>
2019-03-17 13:44:00 -0700

    I'm committing these changes (there isn't much but it asked me to commit changes)

--- front-end/src/components/tools/index.js
+++ front-end/src/components/tools/index.js
@@ -1,9 +1,6 @@
 // Import your component here
 import ExampleTool from './example';
-<<<<<<< HEAD
 import Character from './character';
-=======
->>>>>>> 70c713809f9f7910a33c055e985be7d01b682159
 import diceRoller from './dice-roller';
 
 /**
@@ -30,15 +27,12 @@ export default [
 		component: ExampleTool,
 	},
 	{
-<<<<<<< HEAD
 		name: 'character',
 		displayName: 'Character Tool',
 		defaultLabel: 'Character',
 		component: Character,
 	},
 	{
-=======
->>>>>>> 70c713809f9f7910a33c055e985be7d01b682159
 		name: 'diceRoller',
 		displayName: 'Dice Roll',
 		defaultLabel: 'Dice Roller',
