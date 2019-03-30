af27b565b9d72679fe33d284724a61013d85eac3
Joseph Stewart <joseph.s@nationalschoolproject.com>
2019-03-29 23:24:45 -0700

    Add spotify auth routes

---
+++ back-end/spotify-connector/index.js
@@ -0,0 +1,12 @@
+import * as spotifyRoutes from './SpotifyController';
+
+export default app => {
+	app.route('/api/spotify/authorize')
+		.get(spotifyRoutes.authenticateSpotify);
+
+	app.route('/api/spotify/callback')
+		.get(spotifyRoutes.spotifyCallback);
+
+	app.route('/api/spotify/access-token')
+		.get(spotifyRoutes.getAccessToken);
+};
