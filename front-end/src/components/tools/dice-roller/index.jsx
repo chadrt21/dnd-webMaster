2e05b8626db27c4642a557adf9b6f0c1fe7fe7c8
achiaseed <ashley.m.cheah@biola.edu>
2019-04-04 11:54:55 -0700

    MOar COMMITS

--- front-end/src/components/tools/dice-roller/index.jsx
+++ front-end/src/components/tools/dice-roller/index.jsx
@@ -6,12 +6,13 @@
 import React from 'react';
 import ToolBase from '../ToolBase';
 import styles from './styles.less';
-//<Component className={styles.yourClassName} />
+
 
 import {
-	Alignment,
 	Button,
-	ButtonGroup,	
+	ButtonGroup,
+	InputGroup,
+	Popover,	
 } from '@blueprintjs/core';
 
 
@@ -23,40 +24,70 @@ export default class diceroller extends ToolBase{
 			min: 1,
 			max: 20,
 			number: 1,
+			timesRolled: 1,
+			textInputValue: '',
 		};
 	} 
 	componentDidMount() { /*setting the generated number each time*/
 		this.generateNumber();
+	
 	}
-	changeNumber(){
-		this.setState({
-			min: 1,
-			max: 6,
-			number: 1,
-		});
-		this.generateNumber(); /* after the max "resets" send it to this.generateNumber() */
+	/* max Change */
+	maxChange = (n) => { 
+		this.setState({ 
+			max: n,
+		},
+		this.generateNumber(), 
+		);	
 	}
 
 	/*generate the number*/ 
-	generateNumber = () => this.setState(previousState => ({ number: Math.floor(Math.random()*(previousState.max)+1) }));
+	generateNumber = () => this.setState(
+		previousState => {
+			let number = 0;
+			for(let i = 0; i < previousState.timesRolled; i++)
+			{
+				number += Math.floor(Math.random()*(previousState.max)+1);
+			}
+			return{
+				number,
+			};
+		}
+	);
 	
-	
+	onTextInputChange = (event) => 	
+	{ 
+		const splitText = event.target.value.split('d'); //splits into number of times rolled and max number
+		
+		this.setState({ textInputValue: event.target.value, 
+			timesRolled: splitText[0],
+			max: splitText[1], 
+		});
+	};
+		
 
 	render() {
+		const {
+			textInputValue, 
+		} = this.state;
+
 		return(
 			<div className = "diceRoller"> 
 				<h1 className={styles.h1}> Dice Roller </h1>
-				<div className="dice-button-group">
-					<button type="button" className="rollButton" onClick={ () => this.maxChange(4) }>
-						Roll
-					</button>
-					<button type="button" className="icon-caret-down"  onClick={ () => this.maxChange(6) }>
-						caret here
-					</button>
-				</div>
 				<div className={styles.dice} id = "outputContainer"> 
 					<p id="rNum">{ this.state.number }</p>
 				</div> 
+				<ButtonGroup>
+					<Button intent="primary" onClick={this.generateNumber}>
+						Roll
+					</Button>
+					
+					<Popover modifiers={{ arrow: false }}>
+						<Button icon="caret-down" intent="primary"/>
+						<InputGroup placeholder="ex: 2d8" value={textInputValue} pattern="d1" onChange={this.onTextInputChange}/> 
+					</Popover>
+				</ButtonGroup>
+				
 			</div>
 		);
 	}
