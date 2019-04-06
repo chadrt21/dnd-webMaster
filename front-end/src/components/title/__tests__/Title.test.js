0546674928df2f133de549c042d63b588c93b10b
Joseph Stewart <joseph.s@nationalschoolproject.com>
2019-04-06 14:33:51 -0700

    More specific testing for the title

--- front-end/src/components/title/__tests__/Title.test.js
+++ front-end/src/components/title/__tests__/Title.test.js
@@ -3,8 +3,10 @@ import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Title from '../index';
 
+// Set up the adapter that allows us to test render React components
 configure({ adapter: new Adapter() });
 
+// Set up constants that we will use in testing
 const TITLE_TEXT = 'Here is the title';
 const LEFT_COMPONENT_TEXT = 'I am the left hand component';
 const RIGHT_COMPONENT_TEXT = 'I am the right hand component';
@@ -13,6 +15,7 @@ const CLASS_NAME = 'test-class';
 describe(
 	'TitleComponent',
 	() => {
+		// First make sure that the component will even render
 		it('should render the component', () => {
 			const component = mount(
 				<Title>{TITLE_TEXT}</Title>
@@ -20,66 +23,93 @@ describe(
 			expect(component).toMatchSnapshot();
 		});
 
+		// Then make sure that it is actually rendering what we want it to and only that
 		it('should render the correct text', () => {
 			const component = mount(
 				<Title>{TITLE_TEXT}</Title>
 			);
-			expect(component.props().children).toBe(TITLE_TEXT);
+			expect(
+				component.childAt(0).getElement().props.children.filter(child => child).length
+			).toBe(1);
+			expect(
+				component.text()
+			).toBe(TITLE_TEXT);
 		});
 
+		// Then make sure that it can render a left hand component on the left hand side
 		it('should render the left hand component', () => {
+			const leftComponent = (
+				<p id="left-component">{LEFT_COMPONENT_TEXT}</p>
+			);
 			const component = mount(
 				<Title
-					leftComponent={
-						<p>{LEFT_COMPONENT_TEXT}</p>
-					}
+					leftComponent={leftComponent}
 				>
 					{TITLE_TEXT}
 				</Title>
 			);
-			expect(component.find('p').props().children).toBe(LEFT_COMPONENT_TEXT);
+			expect(
+				component.childAt(0).getElement().props.children.filter(child => child).length
+			).toBe(2);
+			expect(
+				component.childAt(0).getElement().props.children.filter(child => child)[0]
+			).toBe(leftComponent);
+			expect(component.find('p#left-component').props().children).toBe(LEFT_COMPONENT_TEXT);
 		});
 
+		// Then we make sure that it can render a right hand component on the right hand side
 		it('should render the right hand component', () => {
+			const rightComponent = (
+				<p id="right-component">{RIGHT_COMPONENT_TEXT}</p>
+			);
 			const component = mount(
 				<Title
-					rightComponent={
-						<p>{RIGHT_COMPONENT_TEXT}</p>
-					}
+					rightComponent={rightComponent}
 				>
 					{TITLE_TEXT}
 				</Title>
 			);
-			expect(component.find('p').props().children).toBe(RIGHT_COMPONENT_TEXT);
+			expect(
+				component.childAt(0).getElement().props.children.filter(child => child).length
+			).toBe(2);
+			expect(
+				component.childAt(0).getElement().props.children.filter(child => child)[1]
+			).toBe(rightComponent);
+			expect(component.find('p#right-component').props().children).toBe(RIGHT_COMPONENT_TEXT);
 		});
 
+		// Then we make sure that it can render both the right hand component and left hand component
+		// in their proper places
 		it('should render both right hand and left hand component', () => {
+			const rightComponent = (
+				<p id="right-component">{RIGHT_COMPONENT_TEXT}</p>
+			);
+			const leftComponent = (
+				<p id="left-component">{LEFT_COMPONENT_TEXT}</p>
+			);
 			const component = mount(
 				<Title
-					rightComponent={
-						<p className="prop-component">{RIGHT_COMPONENT_TEXT}</p>
-					}
-					leftComponent={
-						<p className="prop-component">{LEFT_COMPONENT_TEXT}</p>
-					}
+					rightComponent={rightComponent}
+					leftComponent={leftComponent}
 				>
 					{TITLE_TEXT}
 				</Title>
 			);
-			let hasRight = false;
-			let hasLeft = false;
-			const pElements = component.find('p.prop-component');
-			pElements.forEach(propComponent => {
-				const propComponentText = propComponent.props().children;
-				if (propComponentText === RIGHT_COMPONENT_TEXT) {
-					hasRight = true;
-				} else if (propComponentText === LEFT_COMPONENT_TEXT) {
-					hasLeft = true;
-				}
-			})
-			expect(hasRight && hasLeft && pElements.length === 2).toBe(true);
+
+			expect(
+				component.childAt(0).getElement().props.children.filter(child => child).length
+			).toBe(3);
+			expect(
+				component.childAt(0).getElement().props.children.filter(child => child)[0]
+			).toBe(leftComponent);
+			expect(
+				component.childAt(0).getElement().props.children.filter(child => child)[2]
+			).toBe(rightComponent);
+			expect(component.find('p#right-component').text()).toBe(RIGHT_COMPONENT_TEXT);
+			expect(component.find('p#left-component').text()).toBe(LEFT_COMPONENT_TEXT)
 		});
 
+		// Then we make sure that it can inherit the className that we pass in through <Title />
 		it('inherits className from props', () => {
 			const component = mount(
 				<Title
@@ -88,7 +118,7 @@ describe(
 					{TITLE_TEXT}
 				</Title>
 			);
-			expect(component.find('h1').hasClass(CLASS_NAME)).toBe(true);
+			expect(component.childAt(0).hasClass(CLASS_NAME)).toBe(true);
 		})
 	}
 );
