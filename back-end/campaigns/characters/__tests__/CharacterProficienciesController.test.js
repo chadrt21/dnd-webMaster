1a5736af238fc323d351daa57ace7bc7d04a9feb
Zachary Leonard <zachl@AD.BIOLA.EDU>
2019-04-11 11:49:36 -0700

    Creates many test.js files Creates all __tests__ folders and text files for all back-end directories.

---
+++ back-end/campaigns/characters/__tests__/CharacterProficienciesController.test.js
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import app from '../../server';
+import testUser from '../../../testing/test-user-object';
+
+// Create the 'user' agent that will be making the test API requests
+// It is important to test from the user's point of view
+const agent = request.agent(app);
+
+const X = '';
+
+// Authenticate the 'user' before each request
+beforeEach(async () => {
+	return await agent.get('/api/auth/login').send();
+})
+
+describe('CharacterProficienciesController', () => {
+
+});
