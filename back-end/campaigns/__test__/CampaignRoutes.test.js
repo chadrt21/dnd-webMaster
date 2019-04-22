657bc75e2450d06e5b936363fdd51098fed3ba47
Chad Ross <chad.geekrunner@gmail.com>
2019-04-22 11:13:28 -0700

    Finished with Campaign testing, I think and hope.

--- back-end/campaigns/__test__/CampaignRoutes.test.js
+++ back-end/campaigns/__test__/CampaignRoutes.test.js
@@ -20,14 +20,13 @@ describe('CampaignRoutes', () => {
 			.send()
 			.expect(200);
 
-		// [{"campaignID": 1,"campaignTitle","test"},{...}]
 		// 2) check if getAllCampaigns return an empty array
-		expect(response.body.lenth()).toBe(0);
+		expect(response.body.length()).toBe(0);
 	});
 
 // Then create a campaign
 	it('will test creation of campaigns', async () => {
-		// 2) test the post campaign creation works
+		// 1) test the post campaign creation works
 		const postResponse = await agent
 			.get('/api/campaigns')
 			.send({
@@ -35,19 +34,29 @@ describe('CampaignRoutes', () => {
 			})
 			.expect(200);
 
-		//
-		expect(postResponse.body).toBe();
+		// [{"campaignID":1,"campaignTitle","test"},{...}]
+		// 2) Expect the response to have the first campaign have the campaignID 1
+		expect(postResponse.body[0].campaignID).toBe(1);
+
+		// 3) Expect the response to have the correct campaign name
+		expect(postResponse.body[0].campaignTitle).toBe('Test');
+
+		// 3) any way to change campaign name?
 	});
 
-//
+// Then test if campaign exists and user has access to campaign
 	it('will test if campaigns exist', async () => {
-		// 3) test if campaigns that were created exists
+		// 1) test if campaigns that were created exists
 		const getResponse = await agent
 			.get('/api/campaigns/1/exists')
 			.send()
 			.expect(200);
 
-		expect(getResponse.body).toBe('1')
+		// 2) Expect one item
+		expect(getResponse.body.length()).toBe('1');
+
+		// 3) Expect {"exists":true} if campaign creation was successful
+		expect(getResponse.body.exist).toBe(true);
 	});
 
 });
